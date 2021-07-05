a8e5e4502e8e0f4a37f73811fa3dc04bdc18e446
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-07-05 10:29:46 +0530

    basic socketio setup

---
+++ socketio101/chat.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const socketio = require('socket.io')
+const app = express()
+app.use(express.static(__dirname + '/public'))
+
+
+const expressServer = app.listen(9000)
+
+const io = socketio(expressServer)
+
+io.on('connection', (socket) => {
+    socket.emit('messageFromServer', { data: 'welcome from Socket Server' })
+
+    socket.on('messageToServer', (dataFromClient) => {
+        console.log(dataFromClient)
+    })
+})
